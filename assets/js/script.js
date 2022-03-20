6b813b92a24453d6ec86a249ded406ae885fa4d9
blairyoung15 <bey99d@gmail.com>
2022-03-20 14:35:28 -0500

    project updates. functional bootstrap, updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,15 @@ var formSubmitHandler = function (event) {
     var cityName = cityInputEl.value.trim();
 
     if (cityName) {
-        getWeatherForCity(cityName);
+        createCityButton(cityName);
+        getLatLon(cityName);
         cityInputEl.value = "";
     } else {
         alert("Please enter a city")
     }
 };
 
-// button created for previously searched city is clicked
+/*// button created for previously searched city is clicked
 var buttonClickHandler = function(event) {
     // get the weather data from the clicked element
     var cityWeather = event.target.getAttribute("");
@@ -24,12 +25,28 @@ var buttonClickHandler = function(event) {
       getPreviousWeatherData(cityWeather);
     }
   };
+  */
 
-/*var getWeatherForCity = function(city) {
-    var lat =
-    var lon = 
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=/" + lat + "/&lon=/" + lon + "/&exclude=minutely,hourly,alerts&appid=a43227cdaf84c90e772c877927f059c5";
-    "https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=a43227cdaf84c90e772c877927f059c5"
+var getLatLon = function(city) {
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=a43227cdaf84c90e772c877927f059c5"
+
+    fetch(apiUrl)
+    .then(function(response) {
+        // request was sucessful 
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function(data){
+                console.log(data);
+                getWeatherForCity(data[0].lat, data[0].lon, data[0].name)
+            });
+        } else {
+            alert('Error: City Not Found');
+        }
+    })
+}
+
+var getWeatherForCity = function(lat, lon, city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly,alerts&appid=a43227cdaf84c90e772c877927f059c5";
 
     
     fetch(apiUrl)
@@ -47,12 +64,45 @@ var buttonClickHandler = function(event) {
     })
     .catch(function(error) {
         alert("Unable to connect to Open Weather");
-    }
-        response.json().then(function(data) {
-            console.log(data);
-        });
+    });
     
 };
+
+var displayWeather = function(data, city) {
+    document.querySelector("#city-searched").textContent= city
+    document.querySelector("#current-date").textContent= moment().format("dddd, MMMM Do YYYY")
+    //document.querySelector("#day-icon").textContent= data.current.weather[0].icon
+    document.querySelector("#today-temp").textContent= data.current.temp
+    document.querySelector("#today-wind").textContent= data.current.wind_speed
+    document.querySelector("#today-humidity").textContent= data.current.humidity
+    document.querySelector("#today-uv").textContent= data.current.uvi
+
+    document.querySelector("#dayOne").textContent= moment().add(1, 'd').format("M/D/YYYY")
+    //document.querySelector("#day-temp-1").textContent= data.day[0].
+
+    document.querySelector("#dayTwo").textContent= moment().add(2, 'd').format("M/D/YYYY")
+
+    document.querySelector("#dayThree").textContent= moment().add(3, 'd').format("M/D/YYYY")
+
+    document.querySelector("#dayFour").textContent= moment().add(4, 'd').format("M/D/YYYY")
+
+    document.querySelector("#dayFive").textContent= moment().add(5, 'd').format("M/D/YYYY")
+    
+}
+
+var createCityButton = function(city) {
+    var newButton = document.createElement("button")
+    newButton.textContent = city
+    newButton.addEventListener("click", function(){
+        getLatLon(city)
+    })
+    document.querySelector("#city-buttons").append(newButton)
+}
+
+/* Weather Icon display
+var icon = ("<img src='http://openweathermap.org/img/w/" + weatherDataIn.weather[0].icon + ".png'>");
+$('#display').html( icon );
+
 
 // saved weather data from search formatted, so when button is clicked, previous data fetched will fill in 
 var getPreviousWeatherData = function()
@@ -63,14 +113,15 @@ var displayTodayForecast = function()
 
 // display 5 day forecast
 var display5Day = function()
+*/
 
 
 
 
 
 // event listeners
-cityFormEl.addEventListener("search", formSubmitHandler);
-cityButtonsEl.addEventListener("click", buttonClickHandler)
-*/
+cityFormEl.addEventListener("submit", formSubmitHandler);
+//cityButtonsEl.addEventListener("click", buttonClickHandler)
+
 
 
